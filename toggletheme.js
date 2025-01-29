ca0264c7d7c16b99c0de4d4d9d708e17534fbdd4
rohan-kharel <111512418+rohan-kharel@users.noreply.github.com>
2025-01-29 16:18:24 +0000

    ver 0.03

--- toggletheme.js
+++ toggletheme.js
@@ -1,10 +1,18 @@
 function toggleTheme() {
-    let theme = document.getElementById('theme');
+    let theme = document.getElementById('header-css');
 
-    if (theme.getAttribute('href') == 'light.css') {
-        theme.setAttribute('href', 'dark.css');
+    if (theme.getAttribute('href') == 'styles/light/header.css') {
+        theme.setAttribute('href', 'styles/dark/header.css');
     } else {
-        theme.setAttribute('href', 'light.css');
+        theme.setAttribute('href', 'styles/light/header.css');
+    }
+
+    let text = document.getElementById('text-css');
+
+    if (text.getAttribute('href') == 'styles/light/text.css') {
+        text.setAttribute('href', 'styles/dark/text.css');
+    } else {
+        text.setAttribute('href', 'styles/light/text.css');
     }
 
     let img = document.getElementById('img');
